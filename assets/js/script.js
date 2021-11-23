ed7838107c8ee534e6887460ff63df8451305341
chamibia <b.chami39@gmail.com>
2021-11-23 11:36:25 -0500

    starting on js

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+//OpenWeather API Key 
+var apiKey = "13ea394a45987d6b455b7b721f41dce7"; 
+var url = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric" + "&APPID=" + apiKey;
+console.log(url);
